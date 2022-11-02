6b763f57a22b7efa0252f0fa6fdd916a129b907b
xstartstaticx <daniel.reiller@icloud.com>
2022-11-02 14:07:29 +0100

    added solution

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import "./App.css";
+import InputForm from "./InputForm";
 
 function App() {
+  const labelIds = {
+    firstName: "First Name",
+    lastName: "Last Name",
+    email: "Email",
+  };
+
   return (
     <div className="App">
-        
+      <InputForm type="text" label={labelIds} />
     </div>
   );
 }
--- src/InputForm.jsx
+++ src/InputForm.jsx
@@ -1,9 +1,18 @@
+import { useId } from "react";
+
 function InputForm(props) {
-  const id = "myId";
+  const id = useId();
+  // console.log(id);
   return (
     <div className="inputForm">
-      <label htmlFor={id}>{props.label}</label>
-      <input id={id} type={props.type} />
+      <label htmlFor={"First Name -" + id}>{props.label.firstName}</label>
+      <input id={"First Name -" + id} type={props.type} />
+
+      <label htmlFor={"Last Name -" + id}>{props.label.lastName}</label>
+      <input id={"Last Name -" + id} type={props.type} />
+
+      <label htmlFor={"Email -" + id}>{props.label.email}</label>
+      <input id={"Email -" + id} type={props.type} />
     </div>
   );
 }
